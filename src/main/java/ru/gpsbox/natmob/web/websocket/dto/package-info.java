6afe450a890c411a5befae2acecff95e4d848c4e
Alex Red <alexcopy@mail.ru>
2018-07-11 16:04:50 +0100

    Generated with JHipster 4.14.4

---
+++ src/main/java/ru/gpsbox/natmob/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package ru.gpsbox.natmob.web.websocket.dto;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,81 @@
+import './vendor.ts';
+
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { GateSharedModule, UserRouteAccessService } from './shared';
+import { GateAppRoutingModule} from './app-routing.module';
+import { GateHomeModule } from './home/home.module';
+import { GateAdminModule } from './admin/admin.module';
+import { GateEntityModule } from './entities/entity.module';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { StateStorageService } from './shared/auth/state-storage.service';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        GateAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        GateSharedModule,
+        GateHomeModule,
+        GateAdminModule,
+        GateEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        PaginationConfig,
+        UserRouteAccessService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [
+                StateStorageService,
+                Injector
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [
+                JhiEventManager
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        }
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class GateAppModule {}
